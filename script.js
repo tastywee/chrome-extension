09b43a7e17aa472925fc111a03628760dbf324f6
Joshua Masangcay <josh.tanay@gmail.com>
2023-12-28 03:28:26 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let myLeads = [];
+let inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function () {
+  ///PUSH THE INPUT VALUE TO THE LEADS ARRAY
+  myLeads.push(inputEl.value);
+  renderLeads();
+  inputEl.value = "";
+});
+
+function renderLeads() {
+  let listItems = "";
+  let inputValue = inputEl.value;
+  console.log(inputValue);
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>";
+    console.log(listItems);
+    //ALTERNATIVE CODE
+    // const listItem = document.createElement("li");
+    // listItem.textContent = myLeads[i];
+    // ulEl.append(listItem);
+  }
+  ulEl.innerHTML = listItems;
+}
