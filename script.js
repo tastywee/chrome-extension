d19c6712c6f501f4db42bdd39ef2fa2d9427dcad
Joshua Masangcay <josh.tanay@gmail.com>
2024-01-05 12:29:34 +0800

    Update CSS and JS
    Added design in leads texts and saving to local storage.

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ let myLeads = [];
 let inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
 
 inputBtn.addEventListener("click", function () {
   ///PUSH THE INPUT VALUE TO THE LEADS ARRAY
   myLeads.push(inputEl.value);
-  renderLeads();
   inputEl.value = "";
+
+  //SAVE TO LOCAL STORAGE
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
+  renderLeads();
+
+  console.log( localStorage.getItem("myLeads"));
 });
 
 function renderLeads() {
@@ -15,8 +23,12 @@ function renderLeads() {
   let inputValue = inputEl.value;
   console.log(inputValue);
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>";
-    console.log(listItems);
+    listItems += `
+      <li>
+        <a href='${myLeads[i]}' target='_blank'>
+          ${myLeads[i]}
+        </a>
+      </li>`;
     //ALTERNATIVE CODE
     // const listItem = document.createElement("li");
     // listItem.textContent = myLeads[i];
