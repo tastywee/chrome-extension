c00fc41340bb9f12d5bc11c71f96715766d42d2b
Joshua Masangcay <111274700+tastywee@users.noreply.github.com>
2024-01-08 11:57:58 +0800

    Update files via upload

--- script.js
+++ script.js
@@ -2,8 +2,45 @@ let myLeads = [];
 let inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 console.log(leadsFromLocalStorage);
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+//RENDER TO DOM
+function render(leads) {
+  let listItems = "";
+  let inputValue = inputEl.value;
+  console.log(inputValue);
+
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+      <li>
+        <a href='${leads[i]}' target='_blank'>
+          ${leads[i]}
+        </a>
+      </li>`;
+
+    //ALTERNATIVE CODE
+    // const listItem = document.createElement("li");
+    // listItem.textContent = myLeads[i];
+    // ulEl.append(listItem);
+  }
+  ulEl.innerHTML = listItems;
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+  //DELETE THE LOCAL STORAGE
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
 
 inputBtn.addEventListener("click", function () {
   ///PUSH THE INPUT VALUE TO THE LEADS ARRAY
@@ -12,27 +49,14 @@ inputBtn.addEventListener("click", function () {
 
   //SAVE TO LOCAL STORAGE
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-
-  renderLeads();
-
-  console.log( localStorage.getItem("myLeads"));
+  render(myLeads);
 });
 
-function renderLeads() {
-  let listItems = "";
-  let inputValue = inputEl.value;
-  console.log(inputValue);
-  for (let i = 0; i < myLeads.length; i++) {
-    listItems += `
-      <li>
-        <a href='${myLeads[i]}' target='_blank'>
-          ${myLeads[i]}
-        </a>
-      </li>`;
-    //ALTERNATIVE CODE
-    // const listItem = document.createElement("li");
-    // listItem.textContent = myLeads[i];
-    // ulEl.append(listItem);
-  }
-  ulEl.innerHTML = listItems;
-}
+tabBtn.addEventListener("click", function () {
+  //GET THE CURRENT TAB URL FOR CHROME EXT
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
